207ee383181b4915a7d17ef65fd7312f0b17744a
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2025-10-08 13:28:14 +0200

    Creating the personality test and profile pages

---
+++ src/app/personality_test/page.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Test() {
+  return (
+    <div>
+      <h1>Test de personnalité</h1>
+    </div>
+  );
+}
---
+++ src/app/profile/page.js
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Profil() {
+  return (
+    <div>
+      <h1>Profil</h1>
+    </div>
+  );
+}
