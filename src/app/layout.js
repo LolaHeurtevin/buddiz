4af859cf61236d523711a52f5a64fcfeb9c77073
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2025-10-20 09:38:37 +0200

    Setting up PWA for Next

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,7 +5,14 @@ import I18nProvider from "../components/I18nProvider";
 export const metadata = {
   title: "Buddiz",
   description: "",
+  manifest: "/manifest.json",
+  themeColor: "#000000",
+  icons: {
+    icon: "/icons/icon-192x192.png",
+    apple: "/icons/icon-512x512.png",
+  },
 };
+
 
 export default function RootLayout({ children }) {
   return (
