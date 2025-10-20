c2693bddf9eba8cc67f0ff43c33cb7910903ad75
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2025-10-20 09:36:34 +0200

    Setting up tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#3d0514",
+        secondary: "#ff80ab",
+        main_bordeau: "#3d0514",
+        "main-bordeau": "#3d0514",
+        mainbordeau: "#3d0514",
+        main_rose: "#ff80ab",
+        "main-rose": "#ff80ab",
+        mainrose: "#ff80ab",
+        main_beige: "#fff8c0",
+        main_beige: "#fff8c0",
+        main_vert: "#218a0a",
+      },
+    },
+  },
+  plugins: [],
+}
