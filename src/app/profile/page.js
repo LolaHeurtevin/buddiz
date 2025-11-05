a1a10bebaa07509bb2ee1d54cc565fa751f5060c
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2025-11-05 11:38:27 +0100

    Button test on profile page

--- src/app/profile/page.js
+++ src/app/profile/page.js
@@ -1,9 +1,17 @@
 "use client";
 
+import Button from "@/components/Button";
+import { useTranslation } from "react-i18next";
+
 export default function Profile() {
+  const { t } = useTranslation();
+
   return (
     <div>
       <h1>Profil</h1>
+      <Button onClick={() => alert("Clicked!")}>
+        {t("settings")}
+      </Button>
     </div>
   );
 }
