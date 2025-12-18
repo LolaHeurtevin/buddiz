fad5cf5dd26517497061df37b6da64fe8b2d46f1
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2025-12-18 10:00:05 +0100

    Deleting personality test access from the bottom nivigation  bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,23 +7,20 @@ export default function Navbar() {
   const { t } = useTranslation();
 
   return (
-    <nav className="app-bottom-nav fixed bottom-0 left-0 right-0 w-full z-[9999] pointer-events-auto bg-main-bordeau text-white"
+    <nav className="app-bottom-nav fixed bottom-0 left-0 right-0 w-full z-[9999] pointer-events-auto bg-white text-main-bordeau"
       style={{ paddingBottom: 'env(safe-area-inset-bottom)', zIndex: 2147483647 }}>
       <div className="max-w-4xl mx-auto flex items-center justify-around p-3">
         <Link href="/" className="text-center text-sm">
-          <i className="bi bi-map text-3xl" aria-label={t("home")}></i>
+          <i className="bi bi-map-fill text-3xl" aria-label={t("home")}></i>
         </Link>
         <Link href="/activities" className="text-center text-sm">
-          <i className="bi bi-pass text-3xl" aria-label={t("activities")}></i>
+          <i className="bi bi-pass-fill text-3xl" aria-label={t("activities")}></i>
         </Link>
         <Link href="/shop" className="text-center text-sm">
-          <i className="bi bi-bag text-3xl" aria-label={t("shop")}></i>
+          <i className="bi bi-bag-fill text-3xl" aria-label={t("shop")}></i>
         </Link>
         <Link href="/profile" className="text-center text-sm">
-          <i className="bi bi-person text-3xl" aria-label={t("profile")}></i>
-        </Link>
-        <Link href="/test" className="text-center text-sm">
-          <i className="bi bi-map text-3xl" aria-label={t("personality test")}></i>
+          <i className="bi bi-person-fill text-3xl" aria-label={t("profile")}></i>
         </Link>
       </div>
     </nav>
