b3c4d293b8c9a4279c7cf11b3d0625e121ed82ed
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2025-12-18 15:29:58 +0100

    Upgrading code by deleting button element and upgrading UI classes

--- src/components/TopButtons.js
+++ src/components/TopButtons.js
@@ -5,46 +5,38 @@ import Link from 'next/link';
 export default function TopButtons() {
     return (
         <div>
-            <Link href="/chat">
-                <button 
-                    style={{
-                        position: 'absolute',
-                        top: '1%',
-                        left: '2%',
-                        zIndex: 1000,
-                    }} 
-                    type="button" 
-                    className={`px-4 py-2 rounded bg-white text-black, border border-grey-200`}>
-                    <i className="bi bi-chat-dots-fill text-3xl" aria-label="Chat"></i>
-                </button>
+            <Link 
+                href="/chat"
+                style={{
+                    position: 'absolute',
+                    top: '5%',
+                    left: '2%',
+                    zIndex: 1000,
+                }} 
+                className={`px-4 py-2 rounded-xl bg-white text-black, border border-grey-200`}
+            >
+                <i className="bi bi-chat-dots-fill text-3xl" aria-label="Chat" />
             </Link>
 
-            <Link href="/test">
-                <button 
-                    style={{
-                        position: 'absolute',
-                        top: '1%',
-                        right: '12%',
-                        zIndex: 1000,
-                    }} 
-                    type="button" 
-                    className={`px-4 py-2 rounded bg-white text-black border border-cta-200`}>
-                    <i className="bi bi-question-circle-fill text-3xl" aria-label="Personality test"></i>
-                </button>
-            </Link>
-            <Link href="/notifications">
-                <button 
-                    style={{
-                        position: 'absolute',
-                        top: '1%',
-                        right: '2%',
-                        zIndex: 1000,
-                    }} 
-                    type="button" 
-                    className={`px-4 py-2 rounded bg-white text-black border border-cta-200`}>
-                    <i className="bi bi-bell-fill text-3xl" aria-label="Notifications"></i>
-                </button>
-            </Link>
+            <div className={`flex flex-row gap-4`}
+                style={{
+                    position: 'absolute',
+                    top: '5%',
+                    right: '2%',
+                    zIndex: 1000,
+                }} 
+            >
+                <Link 
+                    href="/test"
+                    className={`px-4 py-2 rounded-xl bg-white text-black border border-cta-200`}>
+                    <i className="bi bi-question-circle-fill text-3xl" aria-label="Personality test" />
+                </Link>
+                <Link 
+                    href="/notifications"
+                    className={`px-4 py-2 rounded-xl bg-white text-black border border-cta-200`}>
+                    <i className="bi bi-bell-fill text-3xl" aria-label="Notifications" />
+                </Link>
+            </div>
         </div>
     );
 }
