849f1a305e91b25ee125b72591245d3db59ec552
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2025-12-18 15:27:11 +0100

    Using link element instead of router for the redirection and changer UI element

--- src/components/MapButtons.js
+++ src/components/MapButtons.js
@@ -1,28 +1,19 @@
 'use client'
 
-import { useRouter } from 'next/navigation'
-import Image from 'next/image'
+import Link from 'next/link'
 
 export default function MapButtons() {
-
-    const router = useRouter()
-
-    function createActivity() {
-        router.push('/activities/create') 
-    }
-
     return (
-        <button 
+        <Link 
+            href="/activities/create"
             style={{
                 position: 'absolute',
                 bottom: '100px',
                 right: '20px',
                 zIndex: 1000,
             }} 
-            type="button" 
-            onClick={createActivity} 
-            className={`px-4 py-2 rounded bg-cta-200 text-white`}>
-            <i className="bi bi-plus text-3xl" aria-label="Create activity"></i>
-        </button>
+            className={`px-4 py-2 rounded-xl bg-cta-200 text-black`}>
+            <i className="bi bi-plus text-3xl" aria-label="Create activity" />
+        </Link>
     );
 }
