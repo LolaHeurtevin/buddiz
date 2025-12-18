cc67999573ff1c311eca75b42680f4499d45c8e7
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2025-12-18 15:32:35 +0100

    Adding button to the card to add the activity to tyhe users activity list (not wired yet)

--- src/components/Activities/ActivityCard.js
+++ src/components/Activities/ActivityCard.js
@@ -1,27 +1,40 @@
 "use client";
 
 import { useTranslation } from "react-i18next";
-import { useRouter } from "next/navigation";
 import React from "react";
+import Link from "next/link";
 
 export default function ActivityCard({ activity }) {
   const { t } = useTranslation();
-  const router = useRouter(); 
 
-  const handleRedirect = (activity) => {
-    router.push(`/activities/${encodeURIComponent(JSON.stringify(activity.id))}`);
-  };
+  function participate(e) {
+    e.stopPropagation(); // Prevent the click event from propagating to the Link
+    e.preventDefault(); // Prevent default behavior of the Link
+    console.log("participate");
+  }
 
   return (
-    <div
-        key={activity.id}
-        className="p-4 border rounded bg-tertiary-200 shadow-sm cursor-pointer"
-        onClick={() => handleRedirect(activity)}
-    >
-        <h2 className="text-lg font-semibold mb-1">{activity.title}</h2>
-        <p>{t("Max")}: {activity.max_participants} {t("persons")}</p>
-        <p>{t("Start date")}: {activity.start_date}</p>
-        <p>{t("Duration")}: {activity.estimated_duration} {t("hours")}</p>
-    </div>
+    <Link href={`/activities/${encodeURIComponent(JSON.stringify(activity.id))}`} key={activity.id}>
+      <div
+          key={activity.id}
+          className="p-4 border rounded-xl bg-tertiary-200 shadow-sm text-main-bordeau"
+      >
+          <h2 className="mb-1">{activity.title}</h2>
+          <p>{t("Max")}: {activity.max_participants} {t("persons")}</p>
+          <p>{t("Start date")}: {activity.start_date}</p>
+          <p>{t("Duration")}: {activity.estimated_duration} {t("hours")}</p>
+          <button
+            style={{
+              position: 'absolute',
+              bottom: '5%',
+              right: '2%',
+              zIndex: 1000,
+            }} 
+            onClick={participate}
+            className={`px-4 py-2 rounded-xl bg-cta-200 text-black`}>
+              <i className="bi bi-plus text-3xl" aria-label="Participate" />
+          </button>
+      </div>
+    </Link>
   );
 }
