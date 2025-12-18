94e5e2822f6071779015f42ae2268ce8fd7830ba
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2025-12-18 15:33:56 +0100

    Integration the icon section from the mock-up

--- src/components/Activities/ActivityDetail.js
+++ src/components/Activities/ActivityDetail.js
@@ -13,11 +13,37 @@ export default function ActivityDetail({ activity = [] }) {
 
   return (
     <div className="">
-        <h1 className="text-lg font-semibold mb-1">{activity.title}</h1>
-        <p>{t("Max")}: {activity.max_participants} {t("persons")}</p>
-        <p>{t("Start date")}: {activity.start_date}</p>
-        <p>{t("Duration")}: {activity.estimated_duration} {t("hours")}</p>
-        <p>{t("Address")}: {activity.address}, {activity.zip_code} {activity.city}</p>
+        <h1 className="mb-1">{activity.title}</h1>
+        <div className="flex flex-row flex-wrap gap-4 justify-center">
+          <div className="justiy-center items-center text-center">
+            <i className="bi bi-pin-map-fill text-3xl text-main-pink" aria-label={t("Address")} />
+            <p>{activity.address}<br/>{activity.zip_code} {activity.city}</p>
+          </div>
+
+          <div className="justiy-center items-center text-center">
+            <i className="bi bi-people-fill text-3xl text-main-pink" aria-label={t("Max")}/>
+            <p>{activity.max_participants} {t("persons")}</p>
+          </div>
+
+          <div className="justiy-center items-center text-center">
+            <i className="bi bi-calendar-fill text-3xl text-main-pink" aria-label={t("Start date")} />
+            <p>{activity.start_date}</p>
+          </div>
+
+          <div className="justiy-center items-center text-center">
+            <i className="bi bi-clock-fill text-3xl text-main-pink" aria-label={t("Start time")} />
+            <p>{activity.start_time}</p>
+          </div>
+
+          <div className="justiy-center items-center text-center">
+            <i className="bi bi-clock-history text-3xl text-main-pink" aria-label={t("Duration")} />
+            <p>{activity.estimated_duration} {t("hours")}</p>
+          </div>
+        </div>
+
+        <p>{activity.description}</p>
+
+        <h2>{t("Participants")}</h2>
     </div>
   );
 }
