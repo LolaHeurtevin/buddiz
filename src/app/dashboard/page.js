85f66fc73b1c2480ea60b82cbb2116adfa67b0c2
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2026-02-04 16:57:49 +0100

    Setting dashboard title

--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,9 +1,12 @@
 "use client";
 
+import { useTranslation } from "react-i18next";
+
 export default function Dashboard() {
+  const { t } = useTranslation();
   return (
     <div>
-      
+      <h1>{t("My activities")}</h1>
     </div>
   );
 }
