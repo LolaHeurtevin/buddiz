980c5f082f99380e503e88a51aca2891a185903d
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2026-03-12 11:48:24 +0100

    Adding error managment for supabase client

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -3,4 +3,11 @@ import { createClient } from '@supabase/supabase-js'
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 
+if (!supabaseUrl) {
+  throw new Error('NEXT_PUBLIC_SUPABASE_URL is not defined. Check your .env file.')
+}
+if (!supabaseKey) {
+  throw new Error('NEXT_PUBLIC_SUPABASE_ANON_KEY is not defined. Check your .env file.')
+}
+
 export const supabase = createClient(supabaseUrl, supabaseKey)
