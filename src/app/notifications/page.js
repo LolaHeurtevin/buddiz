9e9c64958742da45216f5489350e2259851b3ab7
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2026-03-13 11:26:28 +0100

    Displaying text on notifications page

--- src/app/notifications/page.js
+++ src/app/notifications/page.js
@@ -1,9 +1,14 @@
 "use client";
 
+import { useTranslation } from "react-i18next";
+
 export default function Notifications() {
+  const { t } = useTranslation();
+
   return (
     <div>
-      
+      <h1>{t("Notifications")}</h1>
+      <p>{t("You don't have any new notifications")}</p>
     </div>
   );
 }
