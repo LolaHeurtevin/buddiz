5e4d5678a60808dea47c53fe5f024e4888255266
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2026-03-13 11:25:33 +0100

    Adding status do differenciate the upcoming and past activities

--- src/components/Activities/ActivitiesList.js
+++ src/components/Activities/ActivitiesList.js
@@ -4,7 +4,10 @@ import { useTranslation } from "react-i18next";
 import React from "react";
 import ActivityCard from "./ActivityCard";
 
-export default function ActivitiesList({ data = [] }) {
+/*
+  status: "upcoming" les activités à venir auxquelles on est inscrit | "past" les activités passées auxquelles on a participé | "all" toutes les activités auxquelles on n'est pas inscrits
+*/
+export default function ActivitiesList({ data = [], status="all", reload}) {
   const { t } = useTranslation();
 
   if (data.length === 0) {
@@ -13,9 +16,17 @@ export default function ActivitiesList({ data = [] }) {
 
   return (
     <div className="flex flex-col gap-4">
-      {data.map((activity) => (
-        <ActivityCard key={activity.id} activity={activity} />
-      ))}
+      {
+        status === "all"
+          ? data.map((activity) => (
+              <ActivityCard key={activity.id} activity={activity} status={status} reload={reload} />
+            ))
+          : (status === "upcoming" || status === "past")
+            ? data.map((participation) => (
+                <ActivityCard key={participation.activity.id} activity={participation.activity} status={status} reload={reload}/>
+              ))
+            : null
+      }
     </div>
   );
 }
