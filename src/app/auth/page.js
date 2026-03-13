2760efd239d22521ceafcdec2a5a3ec6d91effe6
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2026-03-13 20:38:57 +0100

    Integrating prototype elements for auth page

--- src/app/auth/page.js
+++ src/app/auth/page.js
@@ -1,5 +1,7 @@
 'use client'
 
+import CustomButton from '@components/CustomButton';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation'
 import { useTranslation } from "react-i18next";
 
@@ -9,17 +11,40 @@ export default function AuthPage() {
   const router = useRouter()
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen gap-6 p-4">
+      <Image
+        src="/buddy/smile.svg"
+        alt="Buddy Smiling"
+        width={200}
+        height={200}
+        className="mx-auto mb-8"
+      />
+      <Image
+        src="/buddiz_white.svg"
+        alt="Buddiz logo"
+        width={200}
+        height={200}
+        className="mx-auto mb-8"
+      />
+
 
       <h1>{t("Your new circle beggins here !")}</h1>
 
-      <button onClick={() => router.push('/auth/login')}>
-        {t("Login")}
-      </button>
+      <CustomButton
+        variant="sign_in" 
+        size="full"   
+        href="/auth/signup" 
+      >
+        {t("I'm signing up")}
+      </CustomButton>
 
-      <button onClick={() => router.push('/auth/signup')}>
-        {t("Sign Up")}
-      </button>
+      <CustomButton
+        variant="bg_white_green_outline" 
+        size="full"   
+        href="/auth/login" 
+      >
+        {t("I'm logging in")}
+      </CustomButton>
 
     </div>
   )
