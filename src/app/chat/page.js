6fd0d18dea3cf714f640c1cb91b60f39e4cbbff8
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2026-03-13 11:27:32 +0100

    Displaying test on chat page

--- src/app/chat/page.js
+++ src/app/chat/page.js
@@ -1,9 +1,14 @@
 "use client";
 
+import { useTranslation } from "react-i18next";
+
 export default function Chat() {
+  const { t } = useTranslation();
+
   return (
     <div>
-      
+      <h1>{t("My conversations")}</h1>
+      <p>{t("You don't have any active conversations right now")}</p>
     </div>
   );
 }
