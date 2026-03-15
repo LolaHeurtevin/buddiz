93002830e1b92661f8d689cbc5c0ab88036fd444
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2026-03-15 14:55:40 +0100

    Intégrating prototype for the bottom part of the onboarding process

--- src/app/onboarding/step2.js
+++ src/app/onboarding/step2.js
@@ -1,23 +1,31 @@
 import { useTranslation } from "react-i18next";
 
-export default function Step2({next}){
+export default function Step2({next, skip}){
   const { t } = useTranslation();
   return(
-
     <div>
+      <div className="bg-red-500 rounded-t-lg p-4 mx-[-34px]">
+        <h3 className="text-white">{t("A safe space to be yourself")}</h3>
 
-      <h1>{t("A safe space to be yourself")}</h1>
+        <p className="text-white">{t("Activities reserved for woman and queer people to help you feel free and at ease")}</p>
 
-      <p>{t("Activities reserved for woman and queer people to help you feel free and at ease")}</p>
+        <div className="flex flex-row gap-4 mt-4">
+          <button 
+            onClick={skip}
+            className="radius-radius-lg bg-grey-0 text-black border-2 border-border-buttons-secondary-default rounded-md py-2 px-4 w-[50%]"
+            >
+            {t("Skip")}
+          </button>
 
-      <button onClick={next}>
-        {t("Skip")}
-      </button>
-
-      <button onClick={next}>
-        {t("Continue")}
-      </button>
-
+          <button 
+            onClick={next}
+            className="bg-beige-600 border-2 border-beige-600 text-black hover:bg-beige-700 rounded-md py-2 px-4 w-[50%]"
+          >
+            {t("Continue")}
+          </button>
+        </div>
+        
+      </div>
     </div>
 
   )
--- src/app/onboarding/step3.js
+++ src/app/onboarding/step3.js
@@ -3,18 +3,29 @@ import { useTranslation } from "react-i18next";
 export default function Step2({next}){
   const { t } = useTranslation();
   return(
-
     <div>
+      <div className="bg-red-500 rounded-t-lg p-4 mx-[-34px]">
+        <h3 className="text-white">{t("Break the ice before taking the plunge")}</h3>
 
-      <h1>{t("Break the ice before taking the plunge")}</h1>
+        <p className="text-white">{t("Answer mini-quizzes with other participants before the activity. No more uneasiness.")}</p>
 
-      <p>{t("Answer mini-quizzes with other participants before the activity. No more uneasiness.")}</p>
+        <div className="flex flex-row gap-4 mt-4">
+          <button 
+            onClick={next}
+            className="radius-radius-lg bg-grey-0 text-black border-2 border-border-buttons-secondary-default rounded-md py-2 px-4 w-[50%]"
+            >
+            {t("Skip")}
+          </button>
 
-      <button onClick={next}>
-        {t("Discover the app")}
-      </button>
-
+          <button 
+            onClick={next}
+            className="bg-beige-600 border-2 border-beige-600 text-black hover:bg-beige-700 rounded-md py-2 px-4 w-[50%]"
+          >
+            {t("Discover the app")}
+          </button>
+        </div>
+        
+      </div>
     </div>
-
   )
 }
