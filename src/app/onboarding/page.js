ff9620b372fd166269851870f18756eeb38d7366
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2026-03-15 14:54:59 +0100

    Adding skipping logic

--- src/app/onboarding/page.js
+++ src/app/onboarding/page.js
@@ -4,16 +4,19 @@ import { useState } from 'react'
 import Step1 from './step1'
 import Step2 from './step2'
 import Step3 from './step3'
+import { useRouter } from 'next/navigation'
 import { useAuth } from '@/lib/useAuth'
 import { useTranslation } from "react-i18next";
 
 export default function Onboarding(){
   const { t } = useTranslation();
+  const router = useRouter()
 
   const [step,setStep] = useState(1)
   const { user, loading } = useAuth()
 
   const next = ()=>setStep(step+1)
+  const skip = ()=> router.push('/')
 
   if (loading) {
     return <p>{t("Loading...")}</p>
@@ -23,8 +26,8 @@ export default function Onboarding(){
     return <p>{t("Redirection to authentication...")}</p>
   }
 
-  if(step===1) return <Step1 next={next}/>
-  if(step===2) return <Step2 next={next}/>
+  if(step===1) return <Step1 next={next} skip={skip}/>
+  if(step===2) return <Step2 next={next} skip={skip}/>
   if(step===3) return <Step3/>
 
 }
