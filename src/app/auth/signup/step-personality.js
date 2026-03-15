f55d92429a20a89e67017899b0d58bbf823405fd
LolaHeurtevin <144373622+LolaHeurtevin@users.noreply.github.com>
2026-03-15 16:40:47 +0100

    Making personality test accessible in the signup process

--- src/app/auth/signup/step-personality.js
+++ src/app/auth/signup/step-personality.js
@@ -1,33 +1,20 @@
 "use client"
 
 import { useEffect, useState } from 'react'
-import { supabase } from '@/lib/supabaseClient'
 import { useRouter } from 'next/navigation'
 import { useTranslation } from "react-i18next";
 import { useAuth } from '@/lib/useAuth'
+import Image from 'next/image';
 
 export default function StepPersonality({formData,setFormData}){
 
   const { t } = useTranslation()
   const router = useRouter()
-  const { user, loading } = useAuth()
 
   const [submitting, setSubmitting] = useState(false)
   const [error, setError] = useState(null)
 
-  // 🔹 redirection automatique
-  useEffect(()=>{
-    if(!loading && !user){
-      router.push('/auth/login')
-    }
-  },[user,loading,router])
-
   const finishSignup = async ()=>{
-
-    if (!user) {
-      setError('Vous devez être connecté pour continuer')
-      return
-    }
 
     setSubmitting(true)
     setError(null)
@@ -45,44 +32,74 @@ export default function StepPersonality({formData,setFormData}){
     }
   }
 
-  if (loading) {
-    return <p>{t("Loading...")}</p>
-  }
-
-  if (!user) {
-    return <p>{t("Redirection to authentication...")}</p>
-  }
-
   return(
-    <div>
+    <div className="flex flex-col justify-between h-full">
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-row">
+          <Image
+            src="/buddy/smile.svg"
+            alt="Buddy Smiling"
+            width={100}
+            height={100}
+            className="mx-auto mb-4"
+          />
+          <div className="flex flex-col">
+            <h3>{t("Shall we check out your profile together ?")}</h3>
+            <p className="text-lg">{t("A few quick questions to get to know you better.")}</p>
+          </div>
+        </div>
 
-      <h2>{t("Personality test")}</h2>
 
-      {error && <p className="text-red-500">{error}</p>}
+        {error && <p className="text-red-500">{error}</p>}
 
-      <button
-        onClick={()=>setFormData({
-          ...formData,
-          personality:[...formData.personality,'sport']
-        })}
-      >
-        Le sport
-      </button>
+        <h3>{t("During your free time, you prefer...")}</h3>
+        <button
+          onClick={()=>setFormData({
+            ...formData,
+            personality:[...formData.personality,'drinks_restaurent']
+          })}
+          className="grey-button"
+        >
+          {t("Getting drinks / going to the restaurent whith friends")}
+        </button>
 
-      <button
-        onClick={()=>setFormData({
-          ...formData,
-          personality:[...formData.personality,'culture']
-        })}
-      >
-        La culture
-      </button>
+        <button
+          onClick={()=>setFormData({
+            ...formData,
+            personality:[...formData.personality,'creative']
+          })}
+          className="grey-button"
+        >
+          {t("A creative activity")}
+        </button>
+
+        <button
+          onClick={()=>setFormData({
+            ...formData,
+            personality:[...formData.personality,'walk']
+          })}
+          className="grey-button"
+        >
+          {t("A walk outdoor")}
+        </button>
+
+        <button
+          onClick={()=>setFormData({
+            ...formData,
+            personality:[...formData.personality,'chill_inside']
+          })}
+          className="grey-button"
+        >
+          {t("A chill activity inside")}
+        </button>
+      </div>
 
       <button 
         onClick={finishSignup}
         disabled={submitting}
+        className="rounded-md bg-green-200 text-black border-2 border-border-buttons-secondary-default"
       >
-        {submitting ? 'En cours...' : 'Terminer'}
+        {submitting ? t("Loading...") : t("Continue")}
       </button>
 
     </div>
